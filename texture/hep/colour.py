5da32accf67fe0b8951844e1015518df3e02c4b4
bianconif <bianco@ieee.org>
2021-10-18 16:40:10 +0200

    Started defining skeleton for colour HEP

---
+++ texture/hep/colour.py
@@ -0,0 +1,18 @@
+from cenotaph.texture.hep.basics import HEP
+
+def HEPColour(HEP):
+    """Base class for three-channel colour descriptors of the HEP family"""
+    
+    def _pre_compute_features(self):
+        
+        #Generate displaced copies of each channel image
+        
+        #*** To be implemented ***
+        
+        #self._gs_layers = matrix_displaced_copies(self._img_in.get_data(), 
+                                                  #self._neighbourhood.\
+                                                  #get_integer_points())         
+        
+
+def OCLBP(HEPColour):
+    """Opponent-colour Local binary patterns"""
